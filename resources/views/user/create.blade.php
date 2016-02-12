1360c957139f752d412f9e93f136070397de2962
slam24 <luismartinez242011@gmail.com>
2016-02-12 05:39:54 -0600

    feat: Partial

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -2,22 +2,9 @@
 @section('content')
 	{!!Form::open(['route'=>'user.store', 'method'=>'POST'])!!}
 		 
-	       <div class="form-group">
-				{!!Form::label('nombre','Nombre:')!!}
-				{!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
-				{{$errors->first('name')}}
-			</div>
-			<div class="form-group">
-				{!!Form::label('email','Correo:')!!}
-				{!!Form::text('email',null,['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
-				{{$errors->first('email')}}
-			</div>
-			<div class="form-group">
-				{!!Form::label('password','Contraseña:')!!}
-				{!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
-				{{$errors->first('password')}}
-			</div>
-			{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+		@include('user.forms.usr')       
+
+	{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
 	
 	{!!Form::close()!!}
 
--- resources/views/user/forms/usr.blade.php
+++ resources/views/user/forms/usr.blade.php
@@ -1,12 +1,15 @@
 	<div class="form-group">
 		{!!Form::label('nombre','Nombre:')!!}
 		{!!Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
+		{{$errors->first('name')}}
 	</div>
 	<div class="form-group">
 		{!!Form::label('email','Correo:')!!}
 		{!!Form::text('email',null,['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
+		{{$errors->first('email')}}
 	</div>
 	<div class="form-group">
 		{!!Form::label('password','Contraseña:')!!}
 		{!!Form::password('password',['class'=>'form-control','placeholder'=>'Ingresa el Nombre del usuario'])!!}
+		{{$errors->first('password')}}
 	</div>
