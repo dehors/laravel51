a6c0a152633f2f37237744e2406cac361ae59a8d
slam24 <luismartinez242011@gmail.com>
2016-02-27 17:52:09 -0600

    feat: read img

--- app/Movie.php
+++ app/Movie.php
@@ -4,6 +4,7 @@ namespace laravel51;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use DB;
 class Movie extends Model
 {
   protected $table = "movies";
@@ -13,5 +14,12 @@ class Movie extends Model
 		$name = Carbon::now()->second.$path->getClientOriginalName();
 		$this->attributes['path'] = $name;
 		\Storage::disk('local')->put($name, \File::get($path));
+	}
+
+	public static function Movies(){
+		return DB::table('movies')
+			->join('genders','genders.id','=','movies.genders_id')
+			->select('movies.*', 'genders.genre')
+			->get();
 	}
 }
